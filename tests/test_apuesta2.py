9d2c82357b763b8d2a6b62d7621b0bd9bb0e8a96
Andres Cardenas <a.cardenasj@uniandes.edu.co>
2022-09-25 00:54:17 -0500

    Actualizar errores con la bd

--- tests/test_apuesta2.py
+++ tests/test_apuesta2.py
@@ -67,19 +67,19 @@ class TestApuesta(TestCase):
         self.assertEqual(nombre_apostador, "Angelica Benitez") '''
 
     def test_GetApuestaById(self):
-        endpoint_apuesta = '/apuesta/{}'.format(7)
+        endpoint_apuesta = '/apuesta/{}'.format(1)
         headers = {'Content-Type': 'application/json'}
 
         solicitud_getAuesta = json.loads(self.client.get(endpoint_apuesta,
                                                    data= '',
                                                    headers=headers).get_data())
         #print(str(solicitud_getAuesta['id_EventoDeportivo']))
-        self.assertAlmostEqual(solicitud_getAuesta['id_EventoDeportivo'], 3)
+        self.assertAlmostEqual(solicitud_getAuesta['id_EventoDeportivo'], 1)
     
     def test_VistaApuestas(self):
         data = {"valor_apostado": 5000,
                 "id_competidor": 3, 
-                "id_apostador" : 2,
+                "id_apostador" : 1,
                 "id_competidor": 4,
                 "id_EventoDeportivo": 3}
         endpoint_apuesta = '/apuestas/{}'.format(data['id_apostador'])
